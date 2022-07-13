232d5f9cd4143c71774f5e4e79917e00b0f44ffb
khumbulani <kerkhood9@gmail.com>
2022-07-13 07:59:39 +0200

    app.js header added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   
+   <H1 align="center"><font style="style=color:darkblue;"> Education is Key</font></H1>
    <h1>shop mall</h1>
     </div>
   );
