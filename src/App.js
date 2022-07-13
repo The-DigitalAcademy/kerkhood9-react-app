798da8c4d2493e207ca92ad7998fc76dd8bcf58c
khumbulani <kerkhood9@gmail.com>
2022-07-13 08:02:10 +0200

    app.js added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <H1 align="center"><font style="style=color:darkblue;"> Education is Key</font></H1>
    <h1>shop mall</h1>
+   
+   <button style="submit">submit</button>
     </div>
   );
 }
