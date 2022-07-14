92a9bb134f4cb221d4c77d439d8b9b659581fdec
khumbulani <kerkhood9@gmail.com>
2022-07-14 07:45:44 +0200

    app.js index.css added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <h1>shop mall</h1>
-   
+    <div className="App"> 
    <div class="topnav">
   <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
+  <a href="#category">Category</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
 </div>
